2c82f06bda333e3dd9dd6a54bdcea9b3794c11ca
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-07-28 00:10:08 +0300

    sol

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,27 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('node:http');
+const { parse } = require('querystring');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-type', 'application/json');
+
+    let [parts, query] = req.url.split('?');
+
+    parts = parts.split('/').filter(Boolean);
+    query = query ? parse(query) : {};
+
+    const response = {
+      parts,
+      query,
+    };
+
+    res.end(JSON.stringify(response));
+  });
+
+  return server;
 }
 
-module.exports = {
-  createServer,
-};
+module.exports = { createServer };
